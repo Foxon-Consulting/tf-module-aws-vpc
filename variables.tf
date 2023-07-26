a6384be948dbc7186e81d78d78ea1847908ca08c
Khalshim <mhalitim@gmail.com>
2023-07-26 18:27:07 +0200

    fix: :art: improve code and variables

--- variables.tf
+++ variables.tf
@@ -7,6 +7,10 @@ variable "availability_zone" {
   description = "The availability zone of the VPC"
   type        = string
   default     = "eu-west-1a"
+  validation {
+    condition     = can(regex("eu-west-1[ab]", var.availability_zone))
+    error_message = "The availability zone must be eu-west-1a or eu-west-1b"
+  }
 }
 
 variable "vpc_isprd" {
@@ -33,7 +37,7 @@ variable "private_subnet_cidr_block" {
   default     = "10.0.2.0/24"
 }
 
-variable "private_rds_subnet_cidr_block" {
+variable "private_subnet_rds_cidr_block" {
   description = "The CIDR block of the second private subnet for RDS"
   type        = string
   default     = "10.0.3.0/24"
@@ -43,4 +47,8 @@ variable "availability_zone_rds" {
   description = "The availability zone of the second RDS subnet"
   type        = string
   default     = "eu-west-1b"
+  validation {
+    condition     = can(regex("eu-west-1[ab]", var.availability_zone_rds))
+    error_message = "The availability zone must be eu-west-1a or eu-west-1b"
+  }
 }
