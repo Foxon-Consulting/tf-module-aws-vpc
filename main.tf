635ef3e931e5e842cef048de431647722cdf04c0
Khalshim <mhalitim@gmail.com>
2023-09-11 15:51:48 +0200

    fix: :pencil2: typo in tag name "client"

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
   tags = {
-    Name    = var.vpc_name
-    isprd   = var.vpc_isprd
-    clientt = var.client_name
+    Name   = var.vpc_name
+    isprd  = var.vpc_isprd
+    client = var.client_name
   }
 }
 
